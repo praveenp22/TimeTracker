e9566d6010558a71db59c545b46c95f0dc37f2aa
Praveen <ppraveen25@gmail.com>
2015-01-13 23:21:30 -0500

    authentication spec update

--- spec/features/authentication_feature_spec.rb
+++ spec/features/authentication_feature_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-describe 'user authentication' do
+describe 'user authentication' do 
 	let(:user) {build(:user)}
-	let!(:account) {create(:account, owner: user)}
+	let!(:account){create(:account, owner: user)}
 
 	it 'allows signin with valid credentials' do
-	sign_user_in(create(:user))
+	sign_user_in(user, subdomain: account.subdomain)
 	expect(page).to have_content('Signed in successfully')
 	end
 
 	it 'does not allows signin with valid credentials' do
-	sign_user_in(create(:user), password: 'wrongpw')
+	sign_user_in(user, subdomain: account.subdomain, password: 'wrongpw')
 	expect(page).to have_content('Invalid email or password')
 	end
 
-	it 'does not allow user to sing in unless on subdomain' do
-		expect {visit new_user_session_path}.to raise_error ActionController:RoutingError
-	end	
+	it 'does not alow user to sign in unless on subdomain' do
+		expect{visit new_user_session_path}.to raise_error ActionController::RoutingError 
+	end
 
 	it 'does allows user to sign out' do	
 	sign_user_in(user, subdomain: account.subdomain)
